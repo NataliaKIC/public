bfd285b019d0bcaf9c9ae84df7d55f7953238877
Natalia <nmozdzynska45@gmail.com>
2022-04-09 14:33:49 +0200

    addddd

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("search").addEventListener("click",() =>{
+
+    document.getElementById("searchinput").style.display="block"
+    document.getElementById("search").src="images/close.png"
+})
+
