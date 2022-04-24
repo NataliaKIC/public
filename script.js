61fbfa20577319ad3573864fdba158a6e057064f
Natalia <nmozdzynska45@gmail.com>
2022-04-24 11:03:32 +0200

    new

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-document.getElementById("search").addEventListener("click",() =>{
+document.getElementById("search").addEventListener("click", () => {
+    el = document.getElementById("searchinput");
+    if (el.style.display == "block") {
+      document.getElementById("searchinput").style.display = "none";
+      document.getElementById("search").src = "images/search.png";
+    } else {
+      console.log(el.style.display);
+      document.getElementById("searchinput").style.display = "flex";
+      document.getElementById("search").src = "images/close.png";
+    }
 
-    document.getElementById("searchinput").style.display="block"
-    document.getElementById("search").src="images/close.png"
-})
+  });
+  
 
